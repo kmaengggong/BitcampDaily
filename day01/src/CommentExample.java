1f49a90325b4fa0ad961bcec88ebd03de2418130
kmaengggong <kmaengggong@gmail.com>
2023-03-28 17:27:11 +0900

    Update day01 directory

---
+++ day01/src/CommentExample.java
@@ -0,0 +1,19 @@
+/**
+ * 이 주석은 ~
+ * 
+ * @author bitcamp1
+ * @since 2023.03.28
+ * @version 0.0.1
+ */
+
+public class CommentExample {
+	public static void main(String[] args) {
+		/*
+		 * 여러 줄 주석
+		 * 여러 줄 주석2
+		 */
+		
+		// 한 줄 주석
+		System.out.println("Hello Bitcamp!");
+	}
+}
---
+++ day01/src/IdentifierExample2.java
@@ -0,0 +1,13 @@
+/**
+ * @author bitcamp1
+ * @since 2023.03.28
+ * @version 0.0.1
+ */
+public class IdentifierExample2 {
+	public static void main(String[] args) {
+		int money = 10000;
+		int salary = 6600000;
+		
+		System.out.println(money + salary + " 원");
+	}
+}
